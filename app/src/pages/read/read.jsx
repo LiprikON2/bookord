cd88fff9865da3c6db574dbb175a9c3fca0246d3
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2022-01-03 08:13:01 +0300

    Flip book pages with arrow keys

--- app/src/pages/read/read.jsx
+++ app/src/pages/read/read.jsx
@@ -7,6 +7,35 @@ import ROUTES from "Constants/routes";
 import "./bookComponent";
 import "./Read.css";
 
+function useEventListener(eventName, handler, element = window) {
+    // Create a ref that stores handler
+    const savedHandler = useRef();
+    // Update ref.current value if handler changes.
+    // This allows our effect below to always get latest handler ...
+    // ... without us needing to pass it in effect deps array ...
+    // ... and potentially cause effect to re-run every render.
+    useEffect(() => {
+        savedHandler.current = handler;
+    }, [handler]);
+    useEffect(
+        () => {
+            // Make sure element supports addEventListener
+            // On
+            const isSupported = element && element.addEventListener;
+            if (!isSupported) return;
+            // Create event listener that calls handler function stored in ref
+            const eventListener = (event) => savedHandler.current(event);
+            // Add event listener
+            element.addEventListener(eventName, eventListener);
+            // Remove event listener on cleanup
+            return () => {
+                element.removeEventListener(eventName, eventListener);
+            };
+        },
+        [eventName, element] // Re-run if eventName or element changes
+    );
+}
+
 const Read = () => {
     const location = useLocation();
     const initStorage = window.api.store.initial();
@@ -62,18 +91,33 @@ const Read = () => {
         bookComponentRef.current = node;
     }, []);
 
+    const handleKeypress = (event) => {
+        if (event.key === "ArrowRight" && event.ctrlKey) {
+            flipNPages(5);
+        } else if (event.key === "ArrowLeft" && event.ctrlKey) {
+            flipNPages(-5);
+        } else if (event.key === "ArrowRight") {
+            goNext();
+        } else if (event.key === "ArrowLeft") {
+            goBack();
+        }
+    };
+    // Add event listener using custom hook
+    useEventListener("keydown", handleKeypress);
+
     const goNext = () => {
-        const bookRef = bookComponentRef.current;
-        const currentPage =
-            bookRef.bookState.getCurrentBookPage(bookRef.contentElem) + 1;
-        const validNextPage = bookRef.enforcePageRange(currentPage + 1);
-        setPage(validNextPage);
+        flipNPages(1);
     };
     const goBack = () => {
+        flipNPages(-1);
+    };
+
+    const flipNPages = (nPageShift) => {
         const bookRef = bookComponentRef.current;
-        const currentPage =
-            bookRef.bookState.getCurrentBookPage(bookRef.contentElem) + 1;
-        const validNextPage = bookRef.enforcePageRange(currentPage - 1);
+        const currentPage = bookRef.bookState.getCurrentBookPage(bookRef) + 1;
+        const validNextPage = bookRef.enforcePageRange(
+            currentPage + nPageShift
+        );
         setPage(validNextPage);
     };
 
