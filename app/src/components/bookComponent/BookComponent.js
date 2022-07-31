33706442852876a28e9b2fd8ffd351c6ac4904fd
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2022-07-31 23:17:01 +0300

    mess: restructure plain js classes into separate files

--- app/src/components/bookComponent/BookComponent.js
+++ app/src/components/bookComponent/BookComponent.js
@@ -1,6 +1,9 @@
 //@ts-check
-///<reference path="../types/index.d.ts" />
+///<reference path="../../pages/types/index.d.ts" />
+///<reference path="typedefs.js" />
+
 import debounce from "lodash/debounce";
+import PageCounter from "./PageCounter";
 
 const style = /*css*/ `
     :host {
@@ -55,128 +58,10 @@ template.innerHTML = /*html*/ `
     </section>
 `;
 
-/** Book file
- * @typedef {Object} BookFile
- * @property {string} name - Book's filename
- * @property {string} path - Path to the book file
- * @property {string} size - Size of the book file in kilobytes
- */
-
-/** Book metadata information
- * @typedef {Object} Info
- * @property {string} title - Book's title
- * @property {Array<string>} idetifiers - List of book's identifiers (such as a UUID, DOI or ISBN)
- * @property {Array<string>} languages - List of language codes that correspond to languages used in the book
- * @property {Array<string>} relations - List of book's relations
- * @property {Array<string>} subjects - List of book's genres
- * @property {Array<string>} publishers - List of book's publishers
- * @property {Array<string>} contributors - List of book's contributors
- * @property {Array<string>} coverages - List of book's coverage information
- * @property {Array<string>} rights - List of book's copyright information
- * @property {Array<string>} sources - List of book's sources
- * @property {string} description - Book's description
- * @property {string} date - Book's publication date (ISO8601)
- * @property {string} cover - Base64-encoded cover image
- * @property {string} author - Author (creator) of the book
- */
-
-/** Dictionary of books which contains information about book file and book metadata.
- * Book filename is used as a key
- * @typedef {Object} AllBooks
- * @property {Book} BookFile.name - Name of the book file, used as a key to the book entry
- */
-
-/** An entry of AllBooks object; contains information about book file and book metadata
- * @typedef {Object} Book
- * @property {BookFile} bookFile - Book file
- * @property {Info} info - Book metadata
- */
-
-/** List of recently visited (and parsed) books, 0th is last opened book
- * @typedef {ParsedBook[]} RecentBooks
- */
-
-/** Book object which contains parsed sections
- * @typedef {Object} ParsedBook
- * @property {Info} info - Book metadata information
- * @property {Array<HtmlObject>} styles - Book's stylesheets
- * @property {HtmlObject} structure - Book's parsed Table Of Contents (TOC)
- * @property {number} sectionsTotal - Book's total number of section
- * @property {Array<string>} sectionNames - List of section ids (names)
- * @property {number} initSectionIndex - Number that corresponds the book's initally parsed section
- * @property {Array<HtmlObject>} initSection - Initially parsed book section
- * @property {Array<HtmlObject>} sections - Sections
- * @property {string} name - Book's filename
- */
-
-/** Book with only one section parsed, used while the rest of the book is being parsed
- * @typedef {Object} InitBook
- * @property {Info} info - Book metadata information
- * @property {Array<HtmlObject>} styles - Book's stylesheets
- * @property {HtmlObject} structure - Book's parsed Table Of Contents (TOC)
- * @property {number} sectionsTotal - Book's total number of section
- * @property {Array<string>} sectionNames - List of section ids (names)
- * @property {number} initSectionIndex - Number that corresponds the book's initally parsed section
- * @property {Array<HtmlObject>} initSection - Initially parsed book section
- * @property {string} name - Book's filename
- */
-
-/** Dictionary of books' bookmarks. Book filename is used as a key to a specific book's bookmark list
- * @typedef {Object} Bookmarks
- * @property {BookmarkList} BookFile.name
- */
-
-/** List of all of the bookmarks a particular book has
- * @typedef {Bookmark[]} BookmarkList
- */
-
-/** A bookmark, used to indicate a specific part of the specific section of the book
- * @typedef {Object} Bookmark
- * @property {number} sectionIndex - Index of the book's section
- * @property {number} elementIndex - Index of descendant elements of contentElem, which is used to mark a specific part of a section
- */
-
-/**
- * @typedef {Object} Event
- * @param {HTMLInputElement} currentTarget
- */
-
-/** HTML & XML parsed to the JavaScript object
- * @typedef {Array<Object>} HtmlObject
- * @property {string|undefined} [name]
- * @property {string|undefined} [text]
- * @property {Attrs|undefined} [attrs]
- * @property {string|undefined} [sectionId]
- * @property {string|undefined} [href]
- * @property {string|undefined} [_data] - HTML content as a string
- * @property {string|undefined} [tag] - HTML tag
- * @property {number|undefined} [type] - Value of 3 corresponds to the text node
- * @property {HtmlObject|undefined} [children]
- */
-
-/** HtmlObject's Attributes
- * @typedef {Object} Attrs
- * @property {string|undefined} [id]
- * @property {string|undefined} [href]
- * @property {string|undefined} [src]
- *
- */
-
-/** Book state for the purposes of displaying
- * @typedef {Object} UIState
- * @property {string} bookTitle
- * @property {string} currentSectionTitle
- * @property {number} currentSectionPage
- * @property {number} totalSectionPages
- * @property {number} currentBookPage
- * @property {number} totalBookPages
- *
- */
-
 /**
  * Book web component
  */
-class BookComponent extends HTMLElement {
+export default class BookComponent extends HTMLElement {
     // Listen for changes in the book-page attribute
     static get observedAttributes() {
         return ["book-page"];
@@ -360,12 +245,15 @@ class BookComponent extends HTMLElement {
      * Handles clicks on book navigation links and website links
      * @param {Event} e - Event
      * @param {InitBook | ParsedBook} book
-     * @listens Event
      * @returns {void}
      */
     handleLink(e, book) {
         e.preventDefault();
-        let [sectionName, markerId] = e.currentTarget.href.split("#").pop().split(",");
+        /**
+         * @type {HTMLAnchorElement | any}
+         */
+        const target = e.currentTarget;
+        let [sectionName, markerId] = target.href.split("#").pop().split(",");
         markerId = "#" + markerId;
 
         const sectionIndex = book.sectionNames.findIndex(
@@ -376,8 +264,8 @@ class BookComponent extends HTMLElement {
             this.loadSection(sectionIndex, 0, markerId);
         } else {
             // Opens link in external browser
-            if (e.currentTarget.href) {
-                window.open(e.currentTarget.href, "_blank");
+            if (target.href) {
+                window.open(target.href, "_blank");
             }
         }
     }
@@ -1094,11 +982,15 @@ class BookComponent extends HTMLElement {
      * @return {void}
      */
     emitImgClickEvent(e) {
+        /**
+         * @type {HTMLImageElement | any}
+         */
+        const target = e.target;
         const imgClickEvent = new CustomEvent("imgClickEvent", {
             bubbles: true,
             cancelable: false,
             composed: true,
-            detail: { src: e.target.src },
+            detail: { src: target.src },
         });
 
         this.dispatchEvent(imgClickEvent);
@@ -1203,105 +1095,3 @@ class BookComponent extends HTMLElement {
 }
 
 window.customElements.define("book-component", BookComponent);
-
-class PageCounter {
-    parentComponent;
-    shadowRoot;
-    #isCounting;
-
-    constructor(bookComponent) {
-        this.parentComponent = bookComponent;
-        this.shadowRoot = bookComponent.shadowRoot;
-        this.#isCounting = false;
-    }
-    get isCounting() {
-        return this.#isCounting;
-    }
-    start() {
-        if (!this.isCounting) {
-            this.#createCounterComponent();
-        }
-    }
-
-    /**
-     * Creates another web component which is used to count pages of a book, and then destroys it
-     * @return {Promise<any>}
-     */
-    async #createCounterComponent() {
-        this.#isCounting = true;
-
-        /** Create a counter`component inside the current component
-         * @type {BookComponent}
-         */
-        // @ts-ignore
-        const childComponent = document.createElement("book-component");
-        this.shadowRoot.appendChild(childComponent);
-
-        // Make it hidden
-        const rootElem = childComponent.shadowRoot.getElementById("root");
-        rootElem.style.visibility = "hidden";
-        rootElem.style.position = "absolute";
-
-        await this.#countBookPages(childComponent);
-
-        this.#isCounting = false;
-        childComponent.remove();
-    }
-
-    /**
-     * Asynchronously and non-blockingly counts pages of a book with a help of a parent web component
-     * @param {BookComponent} childComponent - TODO instance of a parent web component which created this counter web component
-     * @returns {Promise<void>}
-     */
-    async #countBookPages(childComponent) {
-        const book = await this.parentComponent.book;
-
-        this.parentComponent.bookState.sectionPagesArr = [];
-
-        // TODO start counting pages near where user left off (0th bookmark)
-        await this.#asyncForEach(book.sectionNames, async (sectionName, sectionIndex) => {
-            const section = childComponent.getSection(book, sectionIndex);
-            await childComponent.loadStyles(book, section);
-            await childComponent.loadContent(section);
-
-            const totalSectionPages = childComponent.countSectionPages();
-
-            this.parentComponent.bookState.sectionPagesArr.push(totalSectionPages);
-            // Update page count every 10 sections
-            if (sectionIndex % 10 === 0) {
-                this.parentComponent.updateBookUi();
-            }
-            await this.#waitForNextTask();
-        });
-
-        this.parentComponent.updateBookUi();
-    }
-
-    /**
-     * Asynchronous version of a forEach
-     * @param {Array} array
-     * @param {*} callback
-     * @returns {Promise<void>}
-     */
-    async #asyncForEach(array, callback) {
-        for (let index = 0; index < array.length; index++) {
-            await callback(array[index], index, array);
-        }
-    }
-
-    /**
-     * Splits code in chunks
-     * https://stackoverflow.com/a/67135932/10744339
-     * @returns {Promise<void>}
-     */
-    #waitForNextTask() {
-        // @ts-ignore
-        const { port1, port2 } = (this.#waitForNextTask.channel ??= new MessageChannel());
-
-        return new Promise((resolve) => {
-            port1.addEventListener("message", () => resolve(), { once: true });
-            port1.start();
-            port2.postMessage("");
-        });
-    }
-}
