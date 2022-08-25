79138fdb590d4307ecbd40d3bd4a04c1959ba29e
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2022-08-25 03:04:49 +0300

    inc: make segmented control responsive

--- app/src/components/SegmentedControl.jsx
+++ app/src/components/SegmentedControl.jsx
@@ -3,10 +3,14 @@ import { SegmentedControl as MantineSegmentedControl } from "@mantine/core";
 
 import "./SegmentedControl.css";
 
-const SegmentedControl = ({ data, size = undefined, ...rest }) => {
+const SegmentedControl = ({ data, orientation = undefined, className = "", ...rest }) => {
+    const isShort = data.length <= 5;
+    const isVertical = orientation === "vertical" || !isShort;
+
     return (
         <MantineSegmentedControl
-            size={size ?? data.length <= 5 ? null : "xs"}
+            className={className + (isVertical ? " vertical" : "")}
+            orientation={orientation ?? (isShort ? null : "vertical")}
             data={data}
             {...rest}
         />
